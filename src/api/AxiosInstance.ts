777861433cac161ecb1c04044cefb01d10e24b67
EmekaManuel <marrnuel123@gmail.com>
2025-01-23 20:13:57 +0100

    fix(): axios instance

--- src/api/AxiosInstance.ts
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-import { BACKEND_URL } from "@/utils/privateKey";
-
-const instance = axios.create({
-  baseURL: BACKEND_URL,
-  timeout: 240000,
-});
-
-export default instance;
